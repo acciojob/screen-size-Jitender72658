797c3382e15e76adc952dcb73bc50f1c42a4e947
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 15:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-//your JS code here. If required.
-function handleResize() {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-	// let width = document.documentElement.clientWidth;
-    // let height = document.documentElement.clientHeight;
+
+let windowWidth = document.documentElement.clientWidth;
+let windowHeight = document.documentElement.clientHeight;
+let element = document.getElementById('screen-size');
+
+ function handleResize() {
+    windowWidth = document.documentElement.clientWidth;
+    windowHeight = document.documentElement.clientHeight;   
+    let text = `Width: ${windowWidth} and Height: ${windowHeight}`;
+    element.innerText = text;
+   }
+   window.addEventListener('resize', handleResize);
    let text = `Width: ${windowWidth} and Height: ${windowHeight}`;
-   let element = document.getElementById('screen-size');
-      element.innerText = text;  
-  }
-  window.addEventListener('resize', handleResize);
+   element.innerText = text;
